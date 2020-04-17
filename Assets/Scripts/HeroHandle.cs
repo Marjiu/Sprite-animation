1150f57538fdfc6fc09f1a062624a6f09d4ff677
Marjiu <october2363@gmail.com>
2020-04-17 20:40:20 +0800

    設定圖檔和handle

---
+++ Assets/Scripts/HeroHandle.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeroHandle : MonoBehaviour
+{
+    public Sprite[] sprites;
+    private int index = 0;
+    public Image HeroImg;
+    private void FixedUpdate()
+    {
+        index++;
+        if (index >= sprites.Length)
+            index = 0;
+
+    }
+}
